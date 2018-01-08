1ee4e9c94e083a51df36b5affd94d0d5554854d6
khols13 <35153812+khols13@users.noreply.github.com>
2018-01-08 09:36:37 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -107,7 +107,7 @@ wait = {
     'contact':False,
     'autoJoin':True,
     'autoCancel':{"on":False,"members":20},
-    'leaveRoom':True,
+    'leaveRoom':False,
     'timeline':False,
     'autoAdd':True,
     'message':" ",
@@ -1037,7 +1037,7 @@ def bot(op):
                                 print rom
                                 chiya += rom[1] + "\n"
 
-                        cl.sendText(msg.to, "---------------\nActive readers:%s\n\n\n\nPassive readers:\n%s\n\n---------------\nIn the last seen point:\n[%s]\n---------------\n Powered By:ⓗⓒⓣ" % (wait2['readMember'][msg.to],chiya,setTime[msg.to]))
+                        cl.sendText(msg.to, "---------------\nActive readers:%s\n\n\n\nPassive readers:\n%s\n\n---------------\nIn the last seen point:\n[%s]\n---------------\n Powered By: Khols R. Perkasa" % (wait2['readMember'][msg.to],chiya,setTime[msg.to]))
                         print "ReadPoint Set..."
                         try:
                             del wait2['readPoint'][msg.to]
